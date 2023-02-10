dea492300216cc1b21469ce8c99050e6855c5ac9
huchangying <603021137@qq.com>
2023-02-10 16:03:32 +0800

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ module.exports = {
   mode: "production",
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/lib'),
+    library: "boatUIReact",
+    libraryTarget: "umd"
   },
   module: {
     rules: [
