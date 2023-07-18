2b5adaee93b4f92233707b484a515ea8c197fec7
huchangying <603021137@qq.com>
2023-07-18 10:11:32 +0800

    修改test配置

--- lib/index.test.tsx
+++ lib/index.test.tsx
@@ -4,6 +4,8 @@ import Button from "./Button";
 
 describe('button', () => {
   it('should be div ', () => {
-
+    const component = renderer.create(<Button />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
