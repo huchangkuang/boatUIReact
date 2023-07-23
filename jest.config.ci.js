288ba9d8a307e8f7e1978d4ced3b264a005e9b5c
huchangying <603021137@qq.com>
2023-07-23 16:57:48 +0800

    update jest config

---
+++ jest.config.ci.js
@@ -0,0 +1,23 @@
+const base = require('./jest.config')
+
+const config = Object.assign({}, base, {
+  reporters: ["jest-junit"],
+  collectCoverage: true,
+  coverageDirectory: "./coverage",
+  coverageProvider: "v8",
+  coverageThreshold: {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/*.d.ts',
+  ],
+});
+
+module.exports = config;
--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,7 @@
 const config = {
   verbose: true,
-  reporters: ["jest-junit"],
+  reporters: ["default"],
   setupFilesAfterEnv: ["./setupTests.js"],
-  collectCoverage: true,
-  coverageDirectory: "./coverage",
-  coverageProvider: "v8",
-  coverageThreshold: {
-    "global": {
-      "branches": 100,
-      "functions": 100,
-      "lines": 100,
-      "statements": 100
-    }
-  },
-  collectCoverageFrom: [
-    '**/*.{ts,tsx}',
-    '!**/node_modules/**',
-    '!**/*.d.ts',
-  ],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
     "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
