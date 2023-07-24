1e2f7f376a9e72b5528c82287b9ac5fd82a511d5
huchangying <603021137@qq.com>
2023-07-24 18:11:07 +0800

    prettier

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,14 +1,16 @@
-import React, {FC, PropsWithChildren} from "react";
+import React, { FC, PropsWithChildren } from "react";
 import "./index.scss";
 import cs from "classnames";
 import { scopeClassMaker } from "../utils/scopeClassMaker";
 
 const scm = scopeClassMaker("boat-button");
 export interface ButtonProps
-  extends PropsWithChildren<React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-    >> {
+  extends PropsWithChildren<
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      HTMLDivElement
+    >
+  > {
   className?: string;
 }
 const Button: FC<ButtonProps> = ({ className, children, ...rest }) => {
--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,4 +1,4 @@
-import React, {CSSProperties, FC} from "react";
+import React, { CSSProperties, FC } from "react";
 import "./importIcons";
 import "./index.scss";
 import cs from "classnames";
@@ -8,17 +8,26 @@ const scm = scopeClassMaker("boat-icon");
 export interface IconProps extends React.SVGProps<SVGSVGElement> {
   name: string;
   size?: number;
-  color?: CSSProperties['color'];
+  color?: CSSProperties["color"];
 }
 const Icon: FC<IconProps> = (props) => {
-  const { name, className, size, style, color, ...rest } = props
+  const { name, className, size, style, color, ...rest } = props;
   const iconStyle: CSSProperties = {
     width: size,
     height: size,
     fill: color,
-  }
+  };
   return (
-    <svg className={cs(scm(), className)} style={{...style, ...Object.fromEntries(Object.entries(iconStyle).filter(i => i[1] !== undefined))}} {...rest}>
+    <svg
+      className={cs(scm(), className)}
+      style={{
+        ...style,
+        ...Object.fromEntries(
+          Object.entries(iconStyle).filter((i) => i[1] !== undefined),
+        ),
+      }}
+      {...rest}
+    >
       <use xlinkHref={`#${name}`} />
     </svg>
   );
