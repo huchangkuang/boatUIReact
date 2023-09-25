e127f087353dcff46d44aaa8b7651cc3ac4156f5
huchangying <603021137@qq.com>
2023-09-25 22:29:15 +0800

    打包配置，文档编译配置修改

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,11 +1,12 @@
 const base = require('./webpack.config')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
 module.exports = Object.assign({}, base, {
   mode: 'development',
-  entry: './docs/index.tsx',
+  entry: path.resolve(__dirname, '../docs/index.tsx'),
   plugins: [
     new HtmlWebpackPlugin({
-      template: './docs/index.html'
+      template: path.resolve(__dirname, '../docs/index.html')
     })
   ],
 })
--- config/webpack.config.docs.js
+++ config/webpack.config.docs.js
@@ -4,28 +4,14 @@ const path = require("path");
 
 module.exports = Object.assign({}, base, {
   mode: 'production',
-  entry: './docs/index.tsx',
+  entry: path.resolve(__dirname, '../docs/index.tsx'),
   output: {
-    filename: 'index.html',
     path: path.resolve(__dirname, '../build'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './docs/index.html'
+      template: path.resolve(__dirname, '../docs/index.html'),
+      filename: 'doc.html'
     })
-  ],
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM',
-    },
-  },
+  ]
 })
